a467b85aa9756a802956ba4905ad713f5785a41a
Abdallah Bakhet <abakhet2010@gmail.com>
2025-07-08 23:27:18 +0300

    update theme with custom fonts and colors

--- app/src/main/java/com/pixel/caffeine/ui/theme/Color.kt
+++ app/src/main/java/com/pixel/caffeine/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.pixel.caffeine.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val whiteSmoke: Color = Color(0xFFF5F5F5)
+val offWhite: Color = Color(0xFFFFEEE7)
+val kobe: Color = Color(0xFF7C351B)
+val zinnwalditeBrown: Color = Color(0xFF291710)
+val richBlack: Color = Color(0xFF030004)
