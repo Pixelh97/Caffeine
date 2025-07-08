bd67392d89dae0a18b4f0929a31a11904a15d1ee
Abdallah Bakhet <abakhet2010@gmail.com>
2025-07-09 02:39:19 +0300

    remove unnecessary HomeScreen background

--- app/src/main/java/com/pixel/caffeine/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/pixel/caffeine/presentation/screens/HomeScreen.kt
@@ -1,19 +1,14 @@
 package com.pixel.caffeine.presentation.screens
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun HomeScreen(
-    modifier: Modifier = Modifier
-) {
-    Box(modifier.fillMaxSize().background(Color.Green)) {
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Box(modifier) {
         Text(text = "HomeScreen")
     }
 }
