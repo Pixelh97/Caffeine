de9d6aa45c14b14811c39e4b5afd6046b336c21b
Abdallah Bakhet <abakhet2010@gmail.com>
2025-07-09 02:37:35 +0300

    organize packages

---
+++ app/src/main/java/com/pixel/caffeine/presentation/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.pixel.caffeine.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.pixel.caffeine.presentation.theme.CaffeineApp
+import com.pixel.caffeine.presentation.theme.CaffeineTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CaffeineTheme {
+                CaffeineApp()
+            }
+        }
+    }
+}
--- app/src/main/java/com/pixel/caffeine/presentation/theme/Theme.kt
+++ app/src/main/java/com/pixel/caffeine/presentation/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.pixel.caffeine.ui.theme
+package com.pixel.caffeine.presentation.theme
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
